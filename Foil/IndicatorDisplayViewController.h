434acbe410a86180ea906a5d344853e3274850c9
hospitale <rafael.bertholdo@aec.com.br>
2013-10-08 11:52:07 -0300

    removed protocol

--- Foil/IndicatorDisplayViewController.h
+++ Foil/IndicatorDisplayViewController.h
@@ -10,7 +10,7 @@
 #import "IndicatorDisplayCell.h"
 #import "Interaction.h"
 
-@interface IndicatorDisplayViewController : UIViewController<UITabBarControllerDelegate,UICollectionViewDataSource,UICollectionViewDelegate>
+@interface IndicatorDisplayViewController : UIViewController<UICollectionViewDataSource,UICollectionViewDelegate>
 @property (weak, nonatomic) IBOutlet UICollectionView *collectionView;
 @property (nonatomic, readwrite) NSString *referencedSection;
 @end
